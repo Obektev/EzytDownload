0542076e4ce37a66b13443b3ba68a634ce3f454b
Obektev <Obektev@gmail.com>
2022-09-15 14:57:48 +0300

    15.09.22

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
+from pydoc import resolve
 from googleapiclient.discovery import build
 import pandas as pd
 import seaborn as sns
 
 api_key = 'AIzaSyCopCP-3NGlgIitn7Tyd-HS_9U69Z8xA98'
-channel_ids = ['UC7FpXlU6vVA6JkbdnRQBfxg'] # KOSHKINA
-
+#channel_id = 'UCnz-ZXXER4jOvuED5trXfEA'
+#channel_id = input("Input channel url here: ").split("https://www.youtube.com/channel/",1)[1]
+channel_id = "UCh-GSoWgO7_SK7bnV8EBqWQ"
 youtube = build('youtube', 'v3', developerKey=api_key)
 
-def get_channel_stats(youtube, channel_ids):
+def get_channel_stats(youtube, channel_id):
     all_data = []
-    request = youtube.channels().list(
-                part='snippet,contentDetails,statistics',
-                id=','.join(channel_ids))
+    request = youtube.videos().list(
+                part='snippet',
+                id=channel_id)
     response = request.execute() 
-    
+    print(response, "  \n\n\n")
+    print(response['items'])
     for i in range(len(response['items'])):
         data = dict(Channel_name = response['items'][i]['snippet']['title'],
                     Subscribers = response['items'][i]['statistics']['subscriberCount'],
@@ -24,9 +27,5 @@ def get_channel_stats(youtube, channel_ids):
     
     return all_data
 
-channel_statistics = get_channel_stats(youtube, channel_ids)
-channel_data = pd.DataFrame(channel_statistics)
-print(channel_data.to_string())
- 
-
-
+channel_statistics = get_channel_stats(youtube, channel_id)
+print(pd.DataFrame(channel_statistics))
